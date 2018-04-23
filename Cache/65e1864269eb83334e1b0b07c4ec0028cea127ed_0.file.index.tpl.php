f839b4d9680e51257d6f2e8fc769171c0c00b46b
Carlos Verde <cverde@ubiipagos.com>
2018-04-23 19:45:46 -0400

    Sorry, error al llamar el bootstrap... :flushed:

--- Cache/65e1864269eb83334e1b0b07c4ec0028cea127ed_0.file.index.tpl.php
+++ Cache/65e1864269eb83334e1b0b07c4ec0028cea127ed_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2018-04-20 04:45:10
+/* Smarty version 3.1.30, created on 2018-04-24 01:42:55
   from "C:\xampp\htdocs\CandyUCAB\Templates\Public\index.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5ad954366d28b0_93715899',
+  'unifunc' => 'content_5ade6f7f3c65d8_59934593',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '65e1864269eb83334e1b0b07c4ec0028cea127ed' => 
     array (
       0 => 'C:\\xampp\\htdocs\\CandyUCAB\\Templates\\Public\\index.tpl',
-      1 => 1524192309,
+      1 => 1524518402,
       2 => 'file',
     ),
   ),
@@ -23,7 +23,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../Overall/Footer.tpl' => 1,
   ),
 ),false)) {
-function content_5ad954366d28b0_93715899 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ade6f7f3c65d8_59934593 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:../Overall/Header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
