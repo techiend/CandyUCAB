eb1ddef0cd02596a3e05132794f0c3b13f8169cc
Alessandra Varisco <alessandravariscod3@gmail.com>
2018-05-20 21:51:58 -0400

    Caches borrados

--- Cache/4ba639fdb3d43f908046737a1158725acd16bcde_0.file.Header.tpl.php
+++ Cache/4ba639fdb3d43f908046737a1158725acd16bcde_0.file.Header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2018-04-24 04:35:22
+/* Smarty version 3.1.30, created on 2018-05-21 03:41:08
   from "C:\xampp\htdocs\CandyUCAB\Templates\Overall\Header.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5ade97eab886a6_25558406',
+  'unifunc' => 'content_5b0223b4de8bc0_16569754',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '4ba639fdb3d43f908046737a1158725acd16bcde' => 
     array (
       0 => 'C:\\xampp\\htdocs\\CandyUCAB\\Templates\\Overall\\Header.tpl',
-      1 => 1524535466,
+      1 => 1526854481,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ade97eab886a6_25558406 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b0223b4de8bc0_16569754 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <HTML>
@@ -42,7 +42,11 @@ function content_5ade97eab886a6_25558406 (Smarty_Internal_Template $_smarty_tpl)
 
     <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+    <!-- jQuery -->
+    <?php echo '<script'; ?>
+ type="text/javascript" src="assets/jquery/jquery.min.js"><?php echo '</script'; ?>
+>
 
-</head>
-<body><?php }
+
+</head><?php }
 }
